ded1cdc48d7eafe69a73b6c0ca5cb69e56523b55
svey <x@svey.xyz>
2024-10-16 15:04:57 -0400

    Tailwind config relates to #2
    Seperated out core style from tailwind.config to a tailwind.base file in styles. Started to apply css vars to more tailwind classes

--- styles/studio.theme.ts
+++ styles/studio.theme.ts
@@ -4,7 +4,7 @@ const props = {
 	"--custom-white": "#FFF",
 	"--custom-black": "#1A1A1A",
 	"--custom-grey": "#666",
-	"--oam-accent": "#64aca4",
+	"--primary-accent": "#64aca4",
 	"--custom-red": "#C43F21",
 	"--custom-yellow": "#C5A922",
 	"--custom-green": "#22C55E",
@@ -22,17 +22,17 @@ export const studioTheme = buildLegacyTheme({
 	"--component-text-color": props["--custom-white"],
 
 	/* Brand */
-	"--brand-primary": props["--oam-accent"],
+	"--brand-primary": props["--primary-accent"],
 
 	/* Default button */
 	"--default-button-color": props["--custom-grey"],
-	"--default-button-primary-color": props["--oam-accent"],
+	"--default-button-primary-color": props["--primary-accent"],
 	"--default-button-success-color": props["--custom-green"],
 	"--default-button-warning-color": props["--custom-yellow"],
 	"--default-button-danger-color": props["--custom-red"],
 
 	/* State */
-	"--state-info-color": props["--oam-accent"],
+	"--state-info-color": props["--primary-accent"],
 	"--state-success-color": props["--custom-green"],
 	"--state-warning-color": props["--custom-yellow"],
 	"--state-danger-color": props["--custom-red"],
@@ -41,6 +41,6 @@ export const studioTheme = buildLegacyTheme({
 	"--main-navigation-color": props["--custom-black"],
 	"--main-navigation-color--inverted": props["--custom-white"],
 
-	"--focus-color": props["--oam-accent"],
+	"--focus-color": props["--primary-accent"],
 
 })
---
+++ styles/tailwind.base.js
@@ -0,0 +1,90 @@
+/** @type {import('tailwindcss').Config} */
+// Cross project base styles
+
+module.exports = {
+	darkMode: 'class',
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./lib/**/*.{js,ts,jsx,tsx,mdx}',
+		'./styles/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1200px",
+			'2xl': "1440px",
+		},
+		extend: {
+			height: {
+				'icon': 'var(--icon-size)',
+				'icon-sm': 'var(--icon-size-sm)',
+				'header-item': 'var(--header-item)',
+			},
+			width: {
+				'icon': 'var(--icon-size)',
+				'icon-sm': 'var(--icon-size-sm)',
+			},
+			maxWidth: {
+				'prose-full': '85ch',
+				'prose': '65ch',
+				'prose-short': '45ch',
+			},
+			margin: {
+				'1/8': '12%', '1/6': '16%', '1/7': '15%', '1/5': '20%', '1/4': '20%', '1/3': '33%'
+			},
+			colors: {
+				bg: {
+					primary: 'hsl(var(--primary-bg) / <alpha-value>)',
+					DEFAULT: 'hsl(var(--primary-bg) / <alpha-value>)',
+					secondary: 'hsl(var(--secondary-bg) / <alpha-value>)',
+				},
+				fg: {
+					primary: 'hsl(var(--primary-fg) / <alpha-value>)',
+					DEFAULT: 'hsl(var(--primary-fg) / <alpha-value>)',
+				},
+				accent: {
+					primary: 'hsl(var(--primary-accent) / <alpha-value>)',
+					DEFAULT: 'hsl(var(--primary-accent) / <alpha-value>)',
+					secondary: 'hsl(var(--secondary-accent) / <alpha-value>)',
+					warning: 'hsl(var(--warning-accent) / <alpha-value>)',
+					failure: 'hsl(var(--failure-accent) / <alpha-value>)',
+					success: 'hsl(var(--primary-accent) / <alpha-value>)',
+				}
+			},
+			fontFamily: {
+				heading: ['var(--custom-font)'],
+				body: ['var(--custom-font)']
+			},
+			fontSize: {
+				sm: 'calc(var(--text-base-size) * (1 / var(--text-scale)))',
+				base: 'var(--text-base-size)',
+				xl: 'calc(var(--text-base-size) * (1 + 2 * (var(--text-scale) - 1)))',
+				'2xl': 'calc(var(--text-base-size) * (1 + 3 * (var(--text-scale) - 1)))',
+				'3xl': 'calc(var(--text-base-size) * (1 + 4 * (var(--text-scale) - 1 )))',
+				'4xl': 'calc(var(--text-base-size) * (1 + 5 * (var(--text-scale) - 1 )))',
+				'5xl': 'calc(var(--text-base-size) * (1 + 6 * (var(--text-scale) - 1)))',
+			},
+			borderRadius: {
+				'rounded-sm': 'calc(var(--rounded-size) * (1 / var(--rounded-scale)))',
+				'rounded': 'calc(var(--rounded-size))',
+				'rounded-md': 'calc(var(--rounded-size) * (1 + 2 * (var(--rounded-scale) - 1)))',
+				'rounded-lg': 'calc(var(--rounded-size) * (1 + 3 * (var(--rounded-scale) - 1)))',
+				'rounded-xl': 'calc(var(--rounded-size) * (1 + 4 * (var(--rounded-scale) - 1)))',
+				'rounded-2xl': 'calc(var(--rounded-size) * (1 + 5 * (var(--rounded-scale) - 1)))',
+				'rounded-3xl': 'calc(var(--rounded-size) * (1 + 6 * (var(--rounded-scale) - 1)))',
+			},
+			zIndex: {
+				'-1': '-1'
+			},
+		}
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
+}
