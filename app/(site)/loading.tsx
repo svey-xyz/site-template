8199f53d51cc1843bc31dc7c03e015b14821509c
svey-xyz <x@svey.xyz>
2024-11-08 18:49:36 +0000

    Styled loader Relates to #4

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -4,7 +4,8 @@ import React, { ReactNode, Suspense } from 'react';
 // Loading spinner component
 const Spinner = () => (
 	<div className="flex items-center justify-center">
-		<div className="animate-spin rounded-full h-icon w-icon border-t-2 border-accent border-opacity-75"></div>
+		<div className="animate-spin rounded-full h-icon w-icon border-2 border-fg/40 border-solid border-b-accent"></div>
+
 	</div>
 );
 
