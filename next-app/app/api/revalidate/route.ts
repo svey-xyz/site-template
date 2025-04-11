99b6d86dedcab632acf9fbf9be23c02507d1ccb8
svey-xyz <x@svey.xyz>
2025-04-11 13:30:03 -0400

    Remove unused variable import

--- next-app/app/api/revalidate/route.ts
+++ next-app/app/api/revalidate/route.ts
@@ -26,33 +26,33 @@ import { revalidateTag } from 'next/cache'
 import { type NextRequest, NextResponse } from 'next/server'
 import { parseBody } from 'next-sanity/webhook'
 
-import { revalidateSecret } from '@sanity.next-app/lib/api'
+// import { revalidateSecret } from '@sanity.next-app/lib/api'
 
 export async function POST(req: NextRequest) {
 	try {
-		const { body, isValidSignature } = await parseBody<{
-			_type: string
-			slug?: string | undefined
-		}>(req, revalidateSecret)
-		if (!isValidSignature) {
-			const message = 'Invalid signature'
-			return new Response(message, { status: 401 })
-		}
+		// const { body, isValidSignature } = await parseBody<{
+		// 	_type: string
+		// 	slug?: string | undefined
+		// }>(req, revalidateSecret)
+		// if (!isValidSignature) {
+		// 	const message = 'Invalid signature'
+		// 	return new Response(message, { status: 401 })
+		// }
 
-		if (!body?._type) {
-			return new Response('Bad Request', { status: 400 })
-		}
+		// if (!body?._type) {
+		// 	return new Response('Bad Request', { status: 400 })
+		// }
 
-		revalidateTag(body._type)
-		if (body.slug) {
-			revalidateTag(`${body._type}:${body.slug}`)
-		}
-		return NextResponse.json({
-			status: 200,
-			revalidated: true,
-			now: Date.now(),
-			body,
-		})
+		// revalidateTag(body._type)
+		// if (body.slug) {
+		// 	revalidateTag(`${body._type}:${body.slug}`)
+		// }
+		// return NextResponse.json({
+		// 	status: 200,
+		// 	revalidated: true,
+		// 	now: Date.now(),
+		// 	body,
+		// })
 	} catch (err: any) {
 		console.error(err)
 		return new Response(err.message, { status: 500 })
