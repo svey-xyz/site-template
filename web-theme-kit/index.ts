dcc9202a2ce9648d822ecf75f448fb1f6dfa0f24
svey-xyz <x@svey.xyz>
2025-04-14 08:48:43 -0400

    Remove tspath from web-theme-kit index

--- web-theme-kit/index.ts
+++ web-theme-kit/index.ts
@@ -2,4 +2,4 @@ export { EXAMPLES } from "./examples";
 
 export { themeBuilder, getActiveTheme } from "./lib";
 
-export type { WEB_THEME_KIT } from '@types.web-theme-kit/theme';
+export type { WEB_THEME_KIT } from './types/theme';
