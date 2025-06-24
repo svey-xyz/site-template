949c38c5a54a556d3749884162564817980b5cf3
svey-xyz <x@svey.xyz>
2025-06-24 18:49:15 -0400

    Move css var calcs to theme definition

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -7,7 +7,6 @@ const TailwindConfig = {
 		'./lib/**/*.{js,ts,jsx,tsx,mdx}',
 		'./styles/**/*.{js,ts,jsx,tsx,mdx,css}',
 	],
-	darkMode: 'class',
 	future: {
 		hoverOnlyWhenSupported: true,
 	},
@@ -22,8 +21,7 @@ const TailwindConfig = {
 		extend: {
 			height: {
 				'icon': 'var(--icon-base-size)',
-				'icon-sm': 'calc(var(--icon-base-size) * (1 / var(--icon-scale)))',
-				'header-item': 'var(--header-item)',
+				'icon-sm': 'calc(var(--icon-base-size) * (1 / var(--icon-scale)))'
 			},
 			width: {
 				'icon-sm': 'calc(var(--icon-base-size) * (1 / var(--icon-scale)))',
@@ -37,38 +35,7 @@ const TailwindConfig = {
 			margin: {
 				'1/8': '12%', '1/6': '16%', '1/7': '15%', '1/5': '20%', '1/4': '20%', '1/3': '33%'
 			},
-			fontFamily: {
-				heading: ['var(--custom-font)'],
-				body: ['var(--custom-font)']
-			},
-			fontSize: {
-				sm: 'calc(var(--text-base-size) * (1 / var(--text-scale)))',
-				base: 'var(--text-base-size)',
-				xl: 'calc(var(--text-base-size) * (1 + 2 * (var(--text-scale) - 1)))',
-				'2xl': 'calc(var(--text-base-size) * (1 + 3 * (var(--text-scale) - 1)))',
-				'3xl': 'calc(var(--text-base-size) * (1 + 4 * (var(--text-scale) - 1 )))',
-				'4xl': 'calc(var(--text-base-size) * (1 + 5 * (var(--text-scale) - 1 )))',
-				'5xl': 'calc(var(--text-base-size) * (1 + 6 * (var(--text-scale) - 1)))',
-			},
-			borderRadius: {
-				'rounded-sm': 'calc(var(--rounded-size) * (1 / var(--rounded-scale)))',
-				'rounded': 'calc(var(--rounded-size))',
-				'rounded-md': 'calc(var(--rounded-size) * (1 + 2 * (var(--rounded-scale) - 1)))',
-				'rounded-lg': 'calc(var(--rounded-size) * (1 + 3 * (var(--rounded-scale) - 1)))',
-				'rounded-xl': 'calc(var(--rounded-size) * (1 + 4 * (var(--rounded-scale) - 1)))',
-				'rounded-2xl': 'calc(var(--rounded-size) * (1 + 5 * (var(--rounded-scale) - 1)))',
-				'rounded-3xl': 'calc(var(--rounded-size) * (1 + 6 * (var(--rounded-scale) - 1)))',
-			},
-			zIndex: {
-				'-1': '-1'
-			},
-			dropShadow: {
-				'dark': '0px 0px 12px rgba(0, 0, 0, 0.7)',
-				'4xl': [
-					'0 35px 35px rgba(0, 0, 0, 0.25)',
-					'0 45px 65px rgba(0, 0, 0, 0.15)'
-				],
-			},
+			zIndex: { '-1': '-1' },
 			backgroundImage: {
 				'linear-accent-gradient':
 					'linear-gradient(to right, var(--primary-accent), var(--secondary-accent))',
@@ -76,10 +43,6 @@ const TailwindConfig = {
 			},
 		}
 	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-// asd
 } satisfies Config
 
 export default TailwindConfig
