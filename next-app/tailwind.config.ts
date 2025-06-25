6e60834b3489373c3d7c332248de52f7e3f3afa2
svey-xyz <x@svey.xyz>
2025-06-25 00:06:58 -0400

    Transition more of tailwind conf to theme directive

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -19,19 +19,6 @@ const TailwindConfig = {
 			'2xl': "1440px",
 		},
 		extend: {
-			height: {
-				'icon': 'var(--icon-base-size)',
-				'icon-sm': 'calc(var(--icon-base-size) * (1 / var(--icon-scale)))'
-			},
-			width: {
-				'icon-sm': 'calc(var(--icon-base-size) * (1 / var(--icon-scale)))',
-				'icon': 'var(--icon-base-size)',
-			},
-			maxWidth: {
-				'prose-full': '85ch',
-				'prose': '65ch',
-				'prose-short': '45ch',
-			},
 			margin: {
 				'1/8': '12%', '1/6': '16%', '1/7': '15%', '1/5': '20%', '1/4': '20%', '1/3': '33%'
 			},
