7d8b738fdd314ec0197d439348654f3bbf1a231e
svey-xyz <x@svey.xyz>
2025-06-27 18:21:05 -0400

    Remove unused sanity loaders

--- next-app/app/components/Pages/ArticleRoute.tsx
+++ next-app/app/components/Pages/ArticleRoute.tsx
@@ -1,6 +1,5 @@
 import { notFound } from 'next/navigation'
 import dynamic from 'next/dynamic'
-import { loadSingle_Article } from '@sanity.next-app/loader/loader'
 
 type ArticleRouteProps = {
 	type: string,
@@ -18,13 +17,12 @@ const ArticleList: ArticleMap = {
 }
 
 export const ArticleRoute = async ({ type, slug }: ArticleRouteProps) => {
+	return notFound()
+	// const article = null
+	// if (!article) return notFound()
 
-	const data = await loadSingle_Article<any/**article*/>(type, slug)
-	const article = data
-	if (!article) return notFound()
+	// const ArticlePage = ArticleList[article._type] ?? ArticleList.Default
 
-	const ArticlePage = ArticleList[article._type] ?? ArticleList.Default
-
-	return <ArticlePage data={data} />
+	// return <ArticlePage data={data} />
 
 }
