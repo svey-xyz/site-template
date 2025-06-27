840fe438cc7a989078f3b88fdf6eacb3b8b8ec86
svey-xyz <x@svey.xyz>
2025-06-27 15:12:58 -0400

    Add shared lib

--- studio/src/schemas/documents/articles/project.tsx
+++ studio/src/schemas/documents/articles/project.tsx
@@ -1,7 +1,7 @@
 import { camelCaseToWords } from "@lib.studio/stringFunctions";
 import { _ARTICLE_FIELDS } from "@schemas.studio/documents/articles/utils/fields";
 import { _ARTICLE_PREVIEW } from "@schemas.studio/documents/articles/utils/preview";
-import { defineType } from "sanity";
+import { defineField, defineType } from "sanity";
 import { _ARTICLE_TYPES } from "@root.site-template/DocumentTypes";
 import { _ARTICLE_GROUPS } from "@schemas.studio/documents/articles/utils/groups";
 import { CommandLineIcon } from "@heroicons/react/24/solid";
@@ -16,7 +16,13 @@ export const project = defineType({
 	icon: _commandLineIcon,
 	groups: _ARTICLE_GROUPS,
 	fields: [
-		..._ARTICLE_FIELDS
+		..._ARTICLE_FIELDS,
+		defineField({
+			name: 'githubUrl',
+			title: 'GitHub URL',
+			type: 'url',
+			description: 'Link to the project repository on GitHub.',
+		})
 	],
 	preview: _ARTICLE_PREVIEW
 })
