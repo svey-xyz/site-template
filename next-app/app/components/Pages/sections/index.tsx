9e77d80aa5a638ec89b75cc46ff229004b90769e
svey-xyz <x@svey.xyz>
2025-07-02 14:00:47 -0400

    Section grid cols

--- next-app/app/components/Pages/sections/index.tsx
+++ next-app/app/components/Pages/sections/index.tsx
@@ -12,11 +12,27 @@ type ArrElement<ArrType> = ArrType extends readonly (infer ElementType)[]
 type Section = ArrElement<Sections>
 
 export const Section = ({ data, page }: { data: Section, page: Page }) => {
+	const cols = (() => {
+		switch (data.layout) {
+			case "1": return 'grid-cols-[1fr]'
+			case "1-1": return 'grid-cols-[1fr_1fr]'
+			case "1-1-1": return 'grid-cols-[1fr_1fr_1fr]'
+			case "2-1": return 'grid-cols-[2fr_1fr]'
+			case "1-2": return 'grid-cols-[1fr_2fr]'
+			case "2-1-1": return 'grid-cols-[2fr_1fr_1fr]'
+			case "1-2-1": return 'grid-cols-[1fr_2fr_1fr]'
+			case "1-1-2": return 'grid-cols-[1fr_1fr_2fr]'
+			default: return '1fr'
+		}
+	})()
+
 	return (
-		<section id={data.title} className={`relative flex flex-col flex-grow` + data.background == 'opaque' ? `mt-12 border-t border-accent-secondary/40 dark:shadow-xl shadow-lg -mb-[--bottom-spacing] pb-[--bottom-spacing] z-10
-		after:inset-0 after:absolute after:-z-1 after:backdrop-blur-2xl after:bg-gradient-to-b
-		after:from-bg/50 after:to-bg after:to-75% dark:after:from-bg/70 dark:after:to-bg` : ''}>
+		<section id={data.title} className={`relative grid ${cols}`}>
 			<BlockBuilder section={data} page={page} />
 		</section>
 	)
 }
+
+// + data.background == 'opaque' ? `mt-12 border-t border-accent-secondary/40 dark:shadow-xl shadow-lg -mb-[--bottom-spacing] pb-[--bottom-spacing] z-10
+// after:inset-0 after:absolute after:-z-1 after:backdrop-blur-2xl after:bg-gradient-to-b
+// after:from-bg/50 after:to-bg after:to-75% dark:after:from-bg/70 dark:after:to-bg` : ''
