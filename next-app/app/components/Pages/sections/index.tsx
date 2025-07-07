b967782a3c3673a17d1e3733c5339b946d5ebf15
svey-xyz <x@svey.xyz>
2025-07-07 15:59:18 -0400

    Update type imports

--- next-app/app/components/Pages/sections/index.tsx
+++ next-app/app/components/Pages/sections/index.tsx
@@ -25,13 +25,13 @@ export const Section = ({ data, page }: { data: Section, page: Page }) => {
 
 	return (
 		<section id={data.title} className={`relative grid ${cols} gap-6
-			${ (data.background.match('opaque') != null) ?
+			${ (data.background?.match('opaque') != null) ?
 				'mt-12 border-t border-accent-secondary/40 shadow-lg after:inset-0 after:absolute after:-z-1 after:backdrop-blur-2xl after:bg-gradient-to-b after:from-bg/50 after:to-bg after:to-75%' :
 				''
 			}
 		`}>
 			<div className="main-padding my-6">
-				<BlockBuilder section={data} page={page}/>
+				{ data.blocks && <BlockBuilder section={data} page={page}/> }
 			</div>
 		</section>
 	)
